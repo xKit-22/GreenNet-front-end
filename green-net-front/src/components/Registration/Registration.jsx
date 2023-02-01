d100355b38bb9fe73505f5ee4c9d6ad47ae3a1dd
Волошина Александра Андреевна <A.Voloshina@ase-ec.ru>
2023-02-01 16:32:33 +0400

    registration logic works

--- green-net-front/src/components/Registration/Registration.jsx
+++ green-net-front/src/components/Registration/Registration.jsx
@@ -1,8 +1,57 @@
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
 
+import { registration } from '../../redux/userSlice'
 import './registration.scss'
 
 export const Registration = () => {
+    const dispatch = useDispatch();
+
+    const [userLogin, setUserLogin] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [passwordRep, setPasswordRep] = useState("");
+    const [nickname, setNickname] = useState("");
+
+    const [passMatch, setPassMatch] = useState(false);
+    const [errorText, setErrorText] = useState('');
+
+    const [valid, setValid] = useState("");
+
+    const comparePassword = () => {
+        if (userPassword === passwordRep) {
+            setPassMatch(true);
+        } else {
+            setPassMatch(false);
+            setErrorText('Пароли не совпадают!')
+        }
+    }
+
+    const checkFields = () => {
+        if (userLogin && userPassword && nickname && passwordRep) {
+            comparePassword();
+            if (passMatch) {
+                setErrorText("");
+                setValid(true);
+            } else {
+                setErrorText("Пароли не совпадают!");
+                setValid(false);
+            }
+        } else {
+            setErrorText("Заполните поля для входа!");
+            setValid(false);
+        }
+    }
+
+    const toRegister = (e) => {
+        e.preventDefault();
+        checkFields();
+        if (valid) {
+            const data = {userLogin, userPassword, nickname};
+            dispatch(registration(data));
+        } else return;       
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -10,51 +59,35 @@ export const Registration = () => {
 
                 <form>
                     <label htmlFor="">Имя пользователя</label>
-                    <input type="text" 
-                    // onChange={(e) => {
-                    //     setUserName(e.target.value);
-                    // }} 
-                    />
+                    <input type="text" onChange={(e) => {
+                        setNickname(e.target.value);
+                    }}/>
 
                     <label htmlFor="">Email</label>
-                    <input type="text" 
-                    // onChange={(e) => {
-                    //     setEmail(e.target.value);
-                    // }} 
-                    />
+                    <input type="text" onChange={(e) => {
+                        setUserLogin(e.target.value);
+                    }}/>
 
                     <label htmlFor="">Пароль</label>
-                    <input type="password" 
-                    // onChange={(e) => {
-                    //     setPassword(e.target.value);
-                    // }} 
-                    />
+                    <input type="password" onChange={(e) => {
+                        setUserPassword(e.target.value);
+                    }}/>
 
                     <label htmlFor="">Подтвердите пароль</label>
-                    <input type="password" 
-                    // onChange={(e) => {
-                    //     setConfirmPassword(e.target.value);
-                    // }} 
-                    />
+                    <input type="password" onChange={(e) => {
+                        setPasswordRep(e.target.value);
+                    }}/>
 
-                    {/* <p className="error-field">{passMatch ? "" : errorText}</p> */}
-                    <p className="error-field">ошибка</p>
+                    <p className="error-field">{passMatch ? "" : errorText}</p>
 
                     <div className="but-cont">
-                        <button 
-                        // onClick={(e) => toRegister(e)}
-                        >Зарегистрироваться</button>
+                        <button onClick={(e) => toRegister(e)}>Зарегистрироваться</button>
 
                         <Link to="/login" className='log'>
                             <p>Вход</p>
                         </Link>
                     </div>
-
-
-
                 </form>
-
-
             </div>
         </div>
     )
